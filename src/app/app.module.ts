d4f1decadc053a5a522f9e85b4892f55e7adf0d0
Tom <tom@pop-os.localdomain>
2019-10-16 16:14:57 +0200

    /pages deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateProjectComponent } from './pages/create-project/create-project.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateProjectComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
