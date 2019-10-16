4123e34e52aaa0397e218f3891717564538b6e16
Adrien Ideler <adrienideler@Adriens-MacBook-Pro.local>
2019-10-16 17:48:06 +0200

    created: create-delete-edit components for projects-tasks-users folders. | created pages folder.| created models folder. | created user.model.ts, task.model.ts, project.model.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './pages/users/create-user/create-user.component';
+import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
+import { DeleteUserComponent } from './pages/users/delete-user/delete-user.component';
+import { CreateProjectComponent } from './pages/projects/create-project/create-project.component';
+import { EditProjectComponent } from './pages/projects/edit-project/edit-project.component';
+import { DeleteProjectComponent } from './pages/projects/delete-project/delete-project.component';
+import { CreateTaskComponent } from './pages/tasks/create-task/create-task.component';
+import { EditTaskComponent } from './pages/tasks/edit-task/edit-task.component';
+import { DeleteTaskComponent } from './pages/tasks/delete-task/delete-task.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    EditUserComponent,
+    DeleteUserComponent,
+    CreateProjectComponent,
+    EditProjectComponent,
+    DeleteProjectComponent,
+    CreateTaskComponent,
+    EditTaskComponent,
+    DeleteTaskComponent
   ],
   imports: [
     BrowserModule,
