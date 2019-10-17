f4583d39f194975e8932108388cb680037327075
Ben <benwiels@gmail.com>
2019-10-17 10:20:13 +0200

    fix ngserve problem- Adrien

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,78 +1,78 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Project } from '../models/project.model';
-import { Task } from '../models/task.model';
-import { User } from '../models/user.model';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
+// import { Project } from '../models/project.model';
+// import { Task } from '../models/task.model';
+// import { User } from '../models/user.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class RestService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class RestService {
 
-  constructor(private http: HttpClient) { }
+//   constructor(private http: HttpClient) { }
 
   
-  getProjects(): Observable<Project[]>{
-    return this.http.get('http://localhost:3000/projects').pipe(map(data=>data as Project[]));
-  }
-  getProjectByid(id: number): Observable<Project>{
-    return this.http.get(`http://localhost:3000/projects/${id}`).pipe(map(data=>data as Task));
-  }
-  postProject(project: Project): Observable<Project>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.post('http://localhost:3000/projects',project,{headers});
-  }
-  deleteProject(id: number): Observable<Project>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.delete(`http://localhost:3000/projects/${id}`,{headers});
-  }
-  updateProject(id: number): Observable<Project>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.patch(`http://localhost:3000/projects/${id}`,{headers});
-  }
+//   getProjects(): Observable<Project[]>{
+//     return this.http.get('http://localhost:3000/projects').pipe(map(data=>data as Project[]));
+//   }
+//   getProjectByid(id: number): Observable<Project>{
+//     return this.http.get(`http://localhost:3000/projects/${id}`).pipe(map(data=>data as Task));
+//   }
+//   postProject(project: Project): Observable<Project>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.post('http://localhost:3000/projects',project,{headers});
+//   }
+//   deleteProject(id: number): Observable<Project>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.delete(`http://localhost:3000/projects/${id}`,{headers});
+//   }
+//   updateProject(id: number): Observable<Project>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.patch(`http://localhost:3000/projects/${id}`,{headers});
+//   }
 
 
 
-  getTasks(): Observable<Task[]>{
-    return this.http.get('http://localhost:3000/tasks').pipe(map(data=>data as Task[]));
-  }
-  getTaskByid(id: number): Observable<Task>{
-    return this.http.get(`http://localhost:3000/tasks/${id}`).pipe(map(data=>data as Task));
-  }
-  postTask(task: Task): Observable<Task>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.post('http://localhost:3000/tasks',task,{headers});
-  }
-  deleteTask(id: number): Observable<Task>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.delete(`http://localhost:3000/tasks/${id}`,{headers});
-  }
-  updateTask(id: number): Observable<Task>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.patch(`http://localhost:3000/tasks/${id}`,{headers});
-  }
+//   getTasks(): Observable<Task[]>{
+//     return this.http.get('http://localhost:3000/tasks').pipe(map(data=>data as Task[]));
+//   }
+//   getTaskByid(id: number): Observable<Task>{
+//     return this.http.get(`http://localhost:3000/tasks/${id}`).pipe(map(data=>data as Task));
+//   }
+//   postTask(task: Task): Observable<Task>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.post('http://localhost:3000/tasks',task,{headers});
+//   }
+//   deleteTask(id: number): Observable<Task>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.delete(`http://localhost:3000/tasks/${id}`,{headers});
+//   }
+//   updateTask(id: number): Observable<Task>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.patch(`http://localhost:3000/tasks/${id}`,{headers});
+//   }
 
 
 
-  getUsers(): Observable<User[]>{
-    return this.http.get('http://localhost:3000/users').pipe(map(data=>data as User[]));
-  }
-  getUserByid(id: number): Observable<User>{
-    return this.http.get(`http://localhost:3000/users/${id}`).pipe(map(data=>data as User));
-  }
-  postUser(user: User): Observable<User>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.post('http://localhost:3000/users',user,{headers});
-  }
-  deleteUser(id: number): Observable<User>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.delete(`http://localhost:3000/users/${id}`,{headers});
-  }
-  updateUser(id: number): Observable<User>{
-    const headers = new HttpHeaders().set('content-type','application/json');
-    return this.http.patch(`http://localhost:3000/users/${id}`,{headers});
-  }
+//   getUsers(): Observable<User[]>{
+//     return this.http.get('http://localhost:3000/users').pipe(map(data=>data as User[]));
+//   }
+//   getUserByid(id: number): Observable<User>{
+//     return this.http.get(`http://localhost:3000/users/${id}`).pipe(map(data=>data as User));
+//   }
+//   postUser(user: User): Observable<User>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.post('http://localhost:3000/users',user,{headers});
+//   }
+//   deleteUser(id: number): Observable<User>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.delete(`http://localhost:3000/users/${id}`,{headers});
+//   }
+//   updateUser(id: number): Observable<User>{
+//     const headers = new HttpHeaders().set('content-type','application/json');
+//     return this.http.patch(`http://localhost:3000/users/${id}`,{headers});
+//   }
 
-}
+// }
