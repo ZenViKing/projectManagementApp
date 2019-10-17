ecb6f7a9bf3290d48acd896959309a8857431415
Adrien Ideler <adrienideler@Adriens-MacBook-Pro.local>
2019-10-17 15:05:39 +0200

    merged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateUserComponent } from './pages/users/create-user/create-user.component';
-
-
 import { CreateProjectComponent } from './pages/projects/create-project/create-project.component';
 import { EditProjectComponent } from './pages/projects/edit-project/edit-project.component';
-
-
 import { CreateTaskComponent } from './pages/tasks/create-task/create-task.component';
+import { EditTaskComponent } from './pages/tasks/edit-task/edit-task.component';
 
 
 const routes: Routes = [
@@ -31,6 +28,10 @@ const routes: Routes = [
   {
     path: 'task/add',
     component: CreateTaskComponent
+  },
+  {
+    path: 'task/edit',
+    component: EditTaskComponent
   }
 
 ];
