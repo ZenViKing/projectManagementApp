cde1cecb1d33020ebeed85ab7a5ee7984be53eb7
Tom <tom@pop-os.localdomain>
2019-10-17 11:27:24 +0200

    update appRouting Routes create  + Create user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CreateUserComponent} from './pages/users/create-user/create-user.component'
 
 
-const routes: Routes = [];
+import {CreateProjectComponent} from './pages/projects/create-project/create-project.component'
+
+
+
+import {CreateTaskComponent} from './pages/tasks/create-task/create-task.component'
+
+const routes: Routes = [
+//Routes User component 
+{
+path :'user/add',
+component: CreateUserComponent
+},
+
+//Routes Project Components
+{
+  path: 'project/add',
+  component: CreateProjectComponent
+},
+
+//Routes Task Component
+{
+  path: 'task/add',
+  component: CreateTaskComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
