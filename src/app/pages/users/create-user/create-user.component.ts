e313437c870029d9264aee896946f951ac5943f8
Tom <tom@pop-os.localdomain>
2019-10-22 10:58:43 +0200

    upadate create user

--- src/app/pages/users/create-user/create-user.component.ts
+++ src/app/pages/users/create-user/create-user.component.ts
@@ -1,4 +1,8 @@
+import { RestService } from 'src/app/services/rest.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormGroup, FormControl} from '@angular/forms ';
+import {User} from 'src/app/models/user.model';
 
 
 @Component({
@@ -7,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent implements OnInit {
+  user : User;
+  form: FormGroup
 
 
   constructor() { }
