3dabbc0de53a4a1da40bf7ce0f38ad293946a20e
Ben <benwiels@gmail.com>
2019-10-22 12:19:45 +0200

    coded project.model and create-project.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateUserComponent } from './pages/users/create-user/create-user.component';
-import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
+
+import { NavComponent } from './globals/nav/nav.component';
 
 import { CreateProjectComponent } from './pages/projects/create-project/create-project.component';
 import { EditProjectComponent } from './pages/projects/edit-project/edit-project.component';
+import { ListProjectComponent } from './pages/projects/list-project/list-project.component';
 
 import { CreateTaskComponent } from './pages/tasks/create-task/create-task.component';
 import { EditTaskComponent } from './pages/tasks/edit-task/edit-task.component';
-
-import { NavComponent } from './globals/nav/nav.component';
-import { ListProjectComponent } from './pages/projects/list-project/list-project.component';
 import { ListTaskComponent } from './pages/tasks/list-task/list-task.component';
+
+import { CreateUserComponent } from './pages/users/create-user/create-user.component';
+import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
 import { ListUserComponent } from './pages/users/list-user/list-user.component';
+
 
 
 @NgModule({
