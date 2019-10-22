0923444b1866fec84ecac220ec98d170d6a4c3a4
Tom <tom@pop-os.localdomain>
2019-10-22 11:25:12 +0200

    Update Create User#2

--- src/app/pages/users/create-user/create-user.component.ts
+++ src/app/pages/users/create-user/create-user.component.ts
@@ -1,7 +1,7 @@
-import { RestService } from 'src/app/services/rest.service';
-import { Router } from '@angular/router';
+// import { RestService } from 'src/app/services/rest.service';
+// import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import {Validators, FormGroup, FormControl} from '@angular/forms ';
+import {Validators, FormGroup, FormControl} from '@angular/forms';
 import {User} from 'src/app/models/user.model';
 
 
@@ -12,12 +12,21 @@ import {User} from 'src/app/models/user.model';
 })
 export class CreateUserComponent implements OnInit {
   user : User;
-  form: FormGroup
+  form: FormGroup;
 
+  // constructor(private _restService : RestService, private router : Router) {
+  // }
+  constructor() {
+  }
 
-  constructor() { }
 
   ngOnInit() {
+    this.form= new FormGroup({
+      firstname: new FormControl (null, [Validators.required]),
+      lastname: new FormControl (null, [Validators.required]),
+      email: new FormControl (null, [Validators.required, Validators.email]),
+      function:new FormControl (null, [Validators.required])
+    })
   }
 
 }
