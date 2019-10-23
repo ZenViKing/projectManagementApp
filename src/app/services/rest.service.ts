cd7b0ce450ea9e6a031b4b609acfd42966438d6e
Ben <benwiels@gmail.com>
2019-10-23 13:36:19 +0200

    ajout postProject

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -14,57 +14,16 @@ export class RestService {
   constructor(private http: HttpClient) { }
 
 
-<<<<<<< HEAD
-  getProjects(): Observable<Project[]> {
-    return this.http.get('http://localhost:8001/api/projects').pipe(map(data => data as Project[]));
-  }
-  //   getProjectByid(id: number): Observable<Project>{
-  //     return this.http.get(`http://localhost:3000/projects/${id}`).pipe(map(data=>data as Task));
-  //   }
-  postProject(project: Project): Observable<Project> {
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this.http.post('http://localhost:8001/api/projects', project, { headers });
-  }
-  //   deleteProject(id: number): Observable<Project>{
-  //     const headers = new HttpHeaders().set('content-type','application/json');
-  //     return this.http.delete(`http://localhost:3000/projects/${id}`,{headers});
-  //   }
-  //   updateProject(id: number): Observable<Project>{
-  //     const headers = new HttpHeaders().set('content-type','application/json');
-  //     return this.http.patch(`http://localhost:3000/projects/${id}`,{headers});
-  //   }
-
-
-
-  //   getTasks(): Observable<Task[]>{
-  //     return this.http.get('http://localhost:3000/tasks').pipe(map(data=>data as Task[]));
-  //   }
-  //   getTaskByid(id: number): Observable<Task>{
-  //     return this.http.get(`http://localhost:3000/tasks/${id}`).pipe(map(data=>data as Task));
-  //   }
-  //   postTask(task: Task): Observable<Task>{
-  //     const headers = new HttpHeaders().set('content-type','application/json');
-  //     return this.http.post('http://localhost:3000/tasks',task,{headers});
-  //   }
-  //   deleteTask(id: number): Observable<Task>{
-  //     const headers = new HttpHeaders().set('content-type','application/json');
-  //     return this.http.delete(`http://localhost:3000/tasks/${id}`,{headers});
-  //   }
-  //   updateTask(id: number): Observable<Task>{
-  //     const headers = new HttpHeaders().set('content-type','application/json');
-  //     return this.http.patch(`http://localhost:3000/tasks/${id}`,{headers});
-  //   }
-=======
-  listProjects(): Observable<Project[]>{
-    return this.http.get('http://localhost:8001/api/projects/').pipe(map(data=>data as Project[]));
+  listProjects(): Observable<Project[]> {
+    return this.http.get('http://localhost:8001/api/projects/').pipe(map(data => data as Project[]));
   }
   // getProjectByid(id: number): Observable<Project>{
   //   return this.http.get(`http://localhost:3000/projects/${id}`).pipe(map(data=>data as Task));
   // }
-  // postProject(project: Project): Observable<Project>{
-  //   const headers = new HttpHeaders().set('content-type','application/json');
-  //   return this.http.post('http://localhost:3000/projects',project,{headers});
-  // }
+  postProject(project: Project): Observable<Project> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this.http.post('http://localhost:8001/api/projects', project, { headers });
+  }
   // deleteProject(id: number): Observable<Project>{
   //   const headers = new HttpHeaders().set('content-type','application/json');
   //   return this.http.delete(`http://localhost:3000/projects/${id}`,{headers});
@@ -94,7 +53,6 @@ export class RestService {
   //   const headers = new HttpHeaders().set('content-type','application/json');
   //   return this.http.patch(`http://localhost:3000/tasks/${id}`,{headers});
   // }
->>>>>>> 9cb22f03d30d988b9c12fed28715a0d2e52968c8
 
 
 
