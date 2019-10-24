2c43686f4d968db385232f3bfd1e37004cc352b2
Adrien Ideler <adrienideler@Adriens-MacBook-Pro.local>
2019-10-24 10:58:12 +0200

    modif

--- src/app/pages/tasks/list-task/list-task.component.ts
+++ src/app/pages/tasks/list-task/list-task.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Task} from 'src/app/models/task.model';
+import {RestService} from 'src/app/services/rest.service';
 @Component({
   selector: 'app-list-task',
   templateUrl: './list-task.component.html',
   styleUrls: ['./list-task.component.scss']
 })
 export class ListTaskComponent implements OnInit {
-
-  constructor() { }
+  tasks: Task[];
+  constructor(private _restService: RestService) {}
 
   ngOnInit() {
+    this._restService.getTasks().subscribe((data: Task[])=>{
+      this.tasks = data;
+    });
   }
-
 }
