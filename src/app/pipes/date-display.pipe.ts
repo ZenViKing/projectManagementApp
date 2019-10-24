2c22cf0bdee2f22a0e0637b139acd6fc4c992bc9
Ben <benwiels@gmail.com>
2019-10-24 08:55:56 +0200

    création d'un dossier pipes dans app et du pipe pour l'affichage de la date dans listProjects

---
+++ src/app/pipes/date-display.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateDisplay'
+})
+export class DateDisplayPipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    return null;
+  }
+
+}
