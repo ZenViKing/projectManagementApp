d505c1ff3ebcd7684aeeaebb72b2b31be2d9c449
Cheyma Mohamed <mcheyma@pop-os.localdomain>
2019-10-28 09:14:51 +0100

    modif

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { User } from './user.model';
-export class Task {
-    constructor (
-        public name?: string,
-        public status?: string,
-        public assignedUsers?: Array<User[]>,
-        public desc?: string
-=======
 import {User} from './user.model'
 export class Task {
     constructor (
@@ -14,6 +5,5 @@ export class Task {
         public desc?: string,
         public assignedUsers?: Array<User[]>,
         public status?: string
->>>>>>> 2c43686f4d968db385232f3bfd1e37004cc352b2
     ){}
 }
