8956cc2fa4ca0c10ef711ae52cbc10621d97b3bf
Tom <tom@pop-os.localdomain>
2019-10-30 13:29:11 +0100

    Update Users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
-import { MatRadioModule} from '@angular/material/radio';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,10 +25,11 @@ import { ListTaskComponent } from './pages/tasks/list-task/list-task.component';
 import { CreateUserComponent } from './pages/users/create-user/create-user.component';
 import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
 import { ListUserComponent } from './pages/users/list-user/list-user.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { DateDisplayPipe } from './pipes/date-display.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DeleteUserComponent } from './pages/users/delete-user/delete-user.component'
+import { MatCardModule } from '@angular/material';
 
 
 
@@ -42,7 +43,7 @@ import { DeleteUserComponent } from './pages/users/delete-user/delete-user.compo
     CreateTaskComponent,
     EditTaskComponent,
     NavComponent,
-  
+
     ListProjectComponent,
     ListTaskComponent,
     ListUserComponent,
@@ -56,7 +57,7 @@ import { DeleteUserComponent } from './pages/users/delete-user/delete-user.compo
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    // MatCardModule,
+    MatCardModule,
     MatSelectModule,
     MatButtonModule,
     MatRadioModule,
--- src/app/pages/users/list-user/list-user.component.ts
+++ src/app/pages/users/list-user/list-user.component.ts
@@ -1,7 +1,7 @@
 import { RestService } from 'src/app/services/rest.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
-import { ActivatedRoute, Router } from '@angular/router';
+// import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,22 +12,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ListUserComponent implements OnInit {
   users: User[];
 
-  constructor(private _restService: RestService, private router:Router) { }
-
-
-  //  delUser(){
-  //   var txt;
-  //   var r = confirm ("Are you sure that you want to delete this user?");
-  //   if (r==true){
-
-
-
-  // //   else {
-  // //     txt= "Blablabla"
-  // //   }
-
-  // //   }
-  // // }
+  constructor(private _restService: RestService) { }
 
   ngOnInit() {
     this._restService.getUsers().subscribe((data: User[]) => {
